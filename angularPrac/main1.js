b1e2c6680f8f47820f6c6c1a4a4ead6e1a02e12c
Akash Pandya <pandyaakash@gmail.com>
2017-04-30 22:14:00 -0400

    added directives

--- angularPrac/main1.js
+++ angularPrac/main1.js
@@ -26,7 +26,14 @@
 					titleval:"@",//this is passing everything in the form of string instead of object
 					descval:"@"
 				},
-				template:"<div>Title: {{titleval}} and its description: {{descval}}</div>",
+				//template:"<div>Title: {{titleval}} and its description: {{descval}}</div>",
+				template:"<div class='panel panel-success'>" +
+						    "<div class='panel-heading'>" +
+						         "<h3 class='panel-title'>{{titleval}}</h3>" +
+						    "</div>" +
+						    "<div class='panel-body'>{{descval}}</div>" +
+						"</div>",
+
 				link: function (scope, iElement, iAttrs) {
 					console.log(scope.$parent.panelList); // this gives all the data as object of parents
 				}
